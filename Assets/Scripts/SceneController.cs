17864cee8273fde9b488421e2be0c137458cae1c
GameDev Mark <gamedev.markd@gmail.com>
2024-04-03 23:35:27 +0200

    Clear console log in editor function

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +10,18 @@ public class SceneController : MonoBehaviour
     {
         if(Input.GetButtonDown("Jump"))
         {
+            ClearConsole();
             Debug.Log("@Reset scene...");
             SceneManager.LoadScene(0);
         }
+    }
+
+    private void ClearConsole()
+    {
+        // Clear the console log in the Unity editor
+        Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+        Type logEntries = assembly.GetType("UnityEditor.LogEntries");
+        MethodInfo clearMethod = logEntries.GetMethod("Clear");
+        clearMethod.Invoke(new object(), null);
     }
 }
