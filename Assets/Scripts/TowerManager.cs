ba2ed7b812aa0f7ce45f859d578afac1eba69178
GameDev Mark <gamedev.markd@gmail.com>
2024-07-26 17:59:40 +0200

    tower prefabs\\button connections\\tower directory to resources folder\\more..
    - Created tower prefabs (template) primitiveTypes
    - Tower button connections linked
    - Created towerDirectory.cs static class to collect resource folder info
    - TowerManager.cs finding OnMouseClicks, Hover, etc ..
    - Also creating template visuals when tower tile clicked and tower is highlighted..
    - UITowerManager.cs Creating UI sprite icon on mouse position when tower button is clicked
    - Also disclose tower UI info to other script manager

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,41 +1,98 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class TowerManager : MonoBehaviour
 {
+    #region Mouse clicks
     private void OnMouseDown()
     {
-        Debug.Log($"@OnMouseDown {gameObject.name}");
-        CreateTowerGameObject(gameObject);
+        LoopThroughTowerListAndInstaniateCorrectTower();
     }
+    private void OnMouseOver()
+    {
+        TowerVisualWhenHovering();
+    }
+    private void OnMouseExit()
+    {
+        TowerVisualWhenHoveringIsDone();
+    }
+    private void OnMouseEnter()
+    {
+        
+    }
+    #endregion
 
-    private void CreateTowerGameObject(GameObject _towerTile)
+    #region Tower creation
+    private void LoopThroughTowerListAndInstaniateCorrectTower()
     {
         if (!IsTowerTileOccupied())
         {
-            GameObject _tower = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            _tower.name = "Tower";
-            _tower.transform.SetParent(_towerTile.transform);
-            _tower.transform.position = new Vector3(_towerTile.transform.position.x, 1.5f, _towerTile.transform.position.z);
-            _tower.transform.rotation = Quaternion.identity;
-            _tower.transform.localScale = Vector3.one / 2f;
-            GenerateTowerMaterial(_tower, "BridgeTile_CobbleStone");
-            _towerTile.GetComponent<TowerPlacement>().OccupyTile();
+            foreach (var child in TowerDirectory.ReturnListOfTowersFromDirectory())
+            {
+                if (child.name == FindObjectOfType<UITowerManager>().TowerName())
+                {
+                    GameObject _tower = Instantiate(child) as GameObject;
+                    CreateGameObjectInfo(_tower, gameObject);
+                    FindObjectOfType<UITowerManager>().ResetTowerInfo();
+                    StartCoroutine(TowerVisualWhenClickedOn());
+                }
+            }
         }
     }
 
-    private void GenerateTowerMaterial(GameObject _go, string resourceName)
+    private void CreateGameObjectInfo(GameObject _tower, GameObject _towerTile)
     {
-        Renderer rend = _go.GetComponent<Renderer>();
-        rend.material = Resources.Load($"Materials/{resourceName}") as Material;
+        _tower.name = $"Tower_{_tower.name}";
+        _tower.transform.SetParent(_towerTile.transform);
+        _tower.transform.position = new Vector3(_towerTile.transform.position.x, 1.5f, _towerTile.transform.position.z);
+        _tower.transform.rotation = Quaternion.identity;
+        _tower.transform.localScale = Vector3.one / 2f;
+        GenerateTowerMaterial(_tower, "BridgeTile_CobbleStone");
+        _towerTile.GetComponent<TowerPlacement>().OccupyTile();
     }
 
+    private void GenerateTowerMaterial(GameObject _towerGO, string resourceName)
+    {
+        Renderer _rend = _towerGO.GetComponent<Renderer>();
+        _rend.material = Resources.Load($"Materials/{resourceName}") as Material;
+        GetTowerRenderer().material = _rend.material;
+    }
+
+    private Renderer GetTowerRenderer()
+    {
+        return gameObject.GetComponent<Renderer>();
+    }
+    #endregion
+
+    #region Tower visuals
+    private IEnumerator TowerVisualWhenClickedOn()
+    {
+        GetTowerRenderer().material.color = Color.gray;
+        yield return new WaitForSeconds(0.05f);
+        GetTowerRenderer().material.color = Color.black;
+        yield return new WaitForSeconds(0.15f);
+        GetTowerRenderer().material.color = Color.gray;
+        yield return null;
+    }
+
+    private void TowerVisualWhenHovering()
+    {
+        GetTowerRenderer().material.color = Color.yellow;
+    }
+
+    private void TowerVisualWhenHoveringIsDone()
+    {
+        if (!IsTowerTileOccupied())
+            GetTowerRenderer().material.color = Color.white;
+        else
+            GetTowerRenderer().material.color = Color.gray;
+            
+    }
+    #endregion
+
+    #region Public individual tower functionality
     public abstract bool IsTowerTileOccupied();
 
     public abstract void OccupyTile();
-
-    //private IEnumerator OnMouseOver()
-    //{
-    //    Debug.Log($"@Hovering over ");
-    //    yield return new WaitForSeconds(0.5f);
-    //}
+    #endregion
 }
--- Assets/Scripts/UITowerManager.cs
+++ Assets/Scripts/UITowerManager.cs
@@ -1,19 +1,89 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UITowerManager : MonoBehaviour
 {
+    #region Private variables
+    private GameObject towerSprite;
     private Animator towerMenuAnimator;
+    private string currentlyClickedTower = "";
+    #endregion
 
+    #region Public variables
+
+    #endregion
+
+    #region Unity Functions
     private void Awake()
     {
         towerMenuAnimator = GetComponent<Animator>();
+        TowerSpriteIcon();
     }
 
-    public void UIExpandOrShrinkTowerMenu()
+    private void Update()
+    {
+        UpdateTowerImageSpritePosition();
+        CancelTowerClick();
+    }
+    #endregion
+
+    #region Button events
+    public void UIExpandOrShrinkTowerMenu() // attached the UI expand and shrink tower menu button
     {
         if (towerMenuAnimator.GetBool("isTowerMenuOpen"))
             towerMenuAnimator.SetBool("isTowerMenuOpen", false);
         else
             towerMenuAnimator.SetBool("isTowerMenuOpen", true);
     }
+
+    public void UIChooseATower(string _towerName) // attached to the UI tower(s) list button(s)
+    {
+        currentlyClickedTower = _towerName;
+        towerSprite.SetActive(true);
+    }
+    #endregion
+
+    #region Update functionality
+    private void UpdateTowerImageSpritePosition()
+    {
+        if (towerSprite.activeInHierarchy)
+        {
+            towerSprite.transform.position = Input.mousePosition;
+        }
+    }
+
+    private void CancelTowerClick()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            ResetTowerInfo();
+        }
+    }
+    #endregion
+
+    #region Tower information
+    private void TowerSpriteIcon()
+    {
+        towerSprite = new GameObject();
+        towerSprite.name = "Tower Sprite";
+        towerSprite.transform.SetParent(this.gameObject.transform.parent);
+        towerSprite.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        towerSprite.AddComponent<RectTransform>();
+        towerSprite.AddComponent<Image>();
+        towerSprite.GetComponent<Image>().raycastTarget = false;
+        towerSprite.SetActive(false);
+    }
+    #endregion
+
+    #region Public information
+    public string TowerName()
+    {
+        return currentlyClickedTower;
+    }
+    public void ResetTowerInfo()
+    {
+        towerSprite.SetActive(false);
+        currentlyClickedTower = "";
+    }
+    #endregion
 }
